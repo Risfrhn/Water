fc69918b1e7393ba1ca75160a2664f45c6ffe802
Risfrhn <rskyfrhn801@gmail.com>
2026-01-23 15:02:52 +0800

    update: dashboard admin selesai responsive

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,11 +7,23 @@
         <!-- CDN untuk flowbite dan Icon -->
         <link href="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.css" rel="stylesheet" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
-        
+
+        <!-- font -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap" rel="stylesheet">
+
+        <!-- title -->
         <title>Awika Heater || Hangatkan Badanmu</title>
 
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
+
+        <style>
+            body {
+                font-family: 'Varela Round', sans-serif;
+            }
+        </style>
     </head>
     <body class="bg-[#EFF2FB]">
         <livewire-navigasi.sidebar/>
--- resources/views/pages/admin/⚡dashboard.blade.php
+++ resources/views/pages/admin/⚡dashboard.blade.php
@@ -9,93 +9,169 @@ new class extends Component
 ?>
 
 <div>
-    <div class="px-10 py-5 sm:ml-30">
-        <div class="grid grid-cols-12">
-            <div class="col-span-4 content-center">
-                <div class="relative">
-                    <input type="text"
-                        class="border-0 rounded-lg shadow-lg text-gray-400 font-semibold pl-4 pr-4 py-2"
-                        placeholder="Search...">
-
-                    <span class="absolute left-48 top-0 translate-y-1/3 text-gray-400">
+    <div class="px-10 py-5 md:ml-30">
+        <div class="grid grid-cols-12 items-center gap-y-4">
+            <div class="lg:col-span-4 sm:col-span-6 col-span-12 order-2 sm:order-none">
+                <div class="relative w-full sm:w-64">
+                    <input
+                        type="text"
+                        class="w-full py-2 pl-4 pr-10 rounded-lg shadow-lg text-gray-400 font-semibold border-0 focus:ring-0"
+                        placeholder="Search..."
+                    >
+                    <span class="absolute inset-y-0 right-3 flex items-center text-gray-400">
                         <i class="fa-solid fa-magnifying-glass"></i>
                     </span>
                 </div>
             </div>
-            <div class="col-span-4 mx-auto my-auto">
-                <div class="flex flex-row gap-2">
-                    <i class="fa-regular fa-calendar-days mt-0.5"></i>
-                    <p class="text-sm">17 May 2026</p>
+            <div class="lg:col-span-4 hidden lg:flex justify-center">
+                <div class="flex items-center gap-2 text-sm">
+                    <i class="fa-regular fa-calendar-days"></i>
+                    <p>17 May 2026</p>
                 </div>
             </div>
-            <div class="col-span-4 ms-auto">
-                <div class="flex flex-row gap-2">
-                    <i class="fa-regular fa-bell text-[20px] content-center mx-5 text[#424954]"></i>
-                    <img src="/Gambar_default.jpg" alt="" class="rounded-full w-12">
-                    <div class="flex flex-col my-auto">
+            <div class="col-span-6 sm:hidden flex justify-start">
+                <i class="fa-solid fa-bars text-xl"></i>
+            </div>
+            <div class="lg:col-span-4 col-span-6 flex justify-end order-1 sm:order-none">
+                <div class="flex items-center gap-3">
+                    <i class="fa-regular fa-bell text-xl text-[#424954]"></i>
+
+                    <img src="/Gambar_default.jpg" alt=""
+                        class="w-12 h-12 rounded-full object-cover">
+
+                    <div class="hidden md:block">
                         <p class="text-sm font-semibold">Atika Maulida Zahra</p>
                         <p class="text-sm text-gray-400">Admin</p>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="grid grid-cols-12 mt-12">
+
+        <div class="grid grid-cols-12 mt-5 sm:mt-14">
             <div class="col-span-6">
-                <p class="text-3xl font-semibold">Dashboard</p>
+                <p class="text-2xl sm:text-3xl font-semibold">Dashboard</p>
             </div>
             <div class="col-span-6 ms-auto">
-                <button class="py-2 px-10 bg-[#4C7BC3] rounded-lg text-white shadow-lg hover:shadow-[0_10px_25px_rgba(76,123,195,0.55)] transition-shadow duration-300">Buat Baru</button>
+                <button class="py-2 px-10 bg-[#4C7BC3] text-xs sm:text-md rounded-lg text-white shadow-lg hover:shadow-[0_10px_25px_rgba(76,123,195,0.55)] transition-shadow duration-300">Buat Baru</button>
             </div>
         </div>
-        <div class="grid grid-cols-12 mt-12 gap-6">
-            <div class="col-span-8">
-                <div class="py-5 px-5 bg-[#FFFFFF] rounded-2xl shadow-lg">
+        <div class="grid grid-cols-12 mt-5 sm:mt-22 2xl:mt-10 gap-6">
+            <div class="xl:col-span-7 col-span-12">
+                <div class="md:p-5 bg-[#FFFFFF] rounded-2xl shadow-lg">
                     <div class="flex flex-row p-10">
                         <div class="grid grid-cols-12 w-full gap-5">
-                            <div class="col-span-6">
-                                <p class="text-6xl">Hi, Atika!</p>
-                                <p class="text-gray-400 mt-7">Apa yang akan kita lakukan hari ini?</p>
+                            <div class="col-span-12 sm:col-span-6">
+                                <p class="text-3xl md:text-5xl 2xl:text-6xl">Hi, Atika!</p>
+                                <p class="text-gray-400 xl:mt-6.5 2xl:mt-7">Apa yang akan kita lakukan hari ini?</p>
+                                <div class="grid grid-cols-2 gap-y-5 2xl:gap-x-10 xl:gap-x-5 mt-5 xl:mt-10">
+                                    <div class="flex items-center gap-2">
+                                        <i class="fa-regular fa-compass text-red-500"></i>
+                                        <a href="#" class="text-xs 2xl:text-sm hover:underline">Check barang</a>
+                                    </div>
+
+                                    <div class="flex items-center gap-2">
+                                        <i class="fa-regular fa-compass text-blue-500"></i>
+                                        <a href="#" class="text-xs 2xl:text-sm hover:underline">Check barang</a>
+                                    </div>
+
+                                    <div class="flex items-center gap-2">
+                                        <i class="fa-regular fa-compass text-green-500"></i>
+                                        <a href="#" class="text-xs 2xl:text-sm hover:underline">Check barang</a>
+                                    </div>
+
+                                    <div class="flex items-center gap-2">
+                                        <i class="fa-regular fa-compass text-purple-500"></i>
+                                        <a href="#" class="text-xs 2xl:text-sm hover:underline">Check barang</a>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="col-span-6">
-                                <p>Hi, Atika!</p>
-                                <p>Ada berita baik apa hari ini?</p>
+                            <div class="col-span-6 flex items-center justify-center hidden sm:block">
+                                <img src="/Gambar_admin.png" class="object-contain -mt-30 md:-mt-30 xl:-mt-40 w-60 xl:w-74">
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="col-span-4">
+
+
+            <!-- notifikasi -->
+            <div class="xl:col-span-5 col-span-12">
                 <div class="flex flex-row mb-5">
                     <i class="fa-regular fa-bell text-[20px] content-center me-3 text[#424954]"></i>
                     <p class="text-xl">Notifikasi</p>
                     <a href=""class="ms-auto">Lihat semua</a>
                 </div>
-                <div class="py-5 px-5 bg-white shadow-lg rounded-2xl mb-4">
+                <div class="py-3.5 px-3 bg-white shadow-lg rounded-2xl mb-4">
                     <div class="flex flex-row gap-3">
-                        <div class="py-5 px-5 bg-red-50 rounded-2xl">
+                        <div class="py-4 px-4 bg-red-50 rounded-2xl">
                             <i class="fa-regular fa-bell text-[20px] text[#424954]"></i>
                         </div>
-                        <p class="text-lg content-center">Kamu berhasil menbambahkan 1 item barang wika</p>
+                        <p class="text-sm content-center">Kamu berhasil menbambahkan 1 item barang wika</p>
                         <i class="fa-solid fa-trash-can text-lg content-center ms-auto me-5"></i>
                     </div>
                 </div>
-                <div class="py-5 px-5 bg-white shadow-lg rounded-2xl mb-4">
+                <div class="py-3.5 px-3 bg-white shadow-lg rounded-2xl mb-4">
                     <div class="flex flex-row gap-3">
-                        <div class="py-5 px-5 bg-red-50 rounded-2xl">
+                        <div class="py-4 px-4 bg-red-50 rounded-2xl">
                             <i class="fa-regular fa-bell text-[20px] text[#424954]"></i>
                         </div>
-                        <p class="text-lg content-center">Kamu berhasil menbambahkan 1 item barang wika</p>
+                        <p class="text-sm content-center">Kamu berhasil menbambahkan 1 item barang wika</p>
                         <i class="fa-solid fa-trash-can text-lg content-center ms-auto me-5"></i>
                     </div>
                 </div>
-                <div class="py-5 px-5 bg-white shadow-lg rounded-2xl mb-4">
+                <div class="py-3.5 px-3 bg-white shadow-lg rounded-2xl mb-4">
                     <div class="flex flex-row gap-3">
-                        <div class="py-5 px-5 bg-red-50 rounded-2xl">
+                        <div class="py-4 px-4 bg-red-50 rounded-2xl">
                             <i class="fa-regular fa-bell text-[20px] text[#424954]"></i>
                         </div>
-                        <p class="text-lg content-center">Kamu berhasil menbambahkan 1 item barang wika</p>
+                        <p class="text-sm content-center">Kamu berhasil menbambahkan 1 item barang wika</p>
                         <i class="fa-solid fa-trash-can text-lg content-center ms-auto me-5"></i>
                     </div>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- Count -->
+        <div class="grid grid-cols-12 mt-5 gap-6">
+            <div class="xl:col-span-3 md:col-span-6 col-span-12">
+                <div class="p-10 bg-white rounded-2xl shadow-xl">
+                    <div class="flex flex-row">
+                        <i class="fa-solid fa-percent text-6xl text-red-500"></i>
+                        <i class="fa-solid fa-angle-right ms-auto"></i>
+                    </div>
+                    <p class="text-lg font-semibold text-gray-400 mt-5 mb-3">Total Kupon Diskon <br>tersedia di outlet</p>
+                    <p class="text-4xl font-semibold text-[#424954]">100 Item</p>
+                </div>
+            </div>
+            <div class="xl:col-span-3 md:col-span-6 col-span-12">
+                <div class="p-10 bg-white rounded-2xl shadow-xl">
+                    <div class="flex flex-row">
+                        <i class="fa-solid fa-percent text-6xl text-red-500"></i>
+                        <i class="fa-solid fa-angle-right ms-auto"></i>
+                    </div>
+                    <p class="text-lg font-semibold text-gray-400 mt-5 mb-3">Total Kupon Diskon <br>tersedia di outlet</p>
+                    <p class="text-4xl font-semibold text-[#424954]">100 Item</p>
+                </div>
+            </div>
+            <div class="xl:col-span-3 md:col-span-6 col-span-12">
+                <div class="p-10 bg-white rounded-2xl shadow-xl">
+                    <div class="flex flex-row">
+                        <i class="fa-solid fa-percent text-6xl text-red-500"></i>
+                        <i class="fa-solid fa-angle-right ms-auto"></i>
+                    </div>
+                    <p class="text-lg font-semibold text-gray-400 mt-5 mb-3">Total Kupon Diskon <br>tersedia di outlet</p>
+                    <p class="text-4xl font-semibold text-[#424954]">100 Item</p>
+                </div>
+            </div>
+            <div class="xl:col-span-3 md:col-span-6 col-span-12">
+                <div class="p-10 bg-white rounded-2xl shadow-xl">
+                    <div class="flex flex-row">
+                        <i class="fa-solid fa-percent text-6xl text-red-500"></i>
+                        <i class="fa-solid fa-angle-right ms-auto"></i>
+                    </div>
+                    <p class="text-lg font-semibold text-gray-400 mt-5 mb-3">Total Kupon Diskon <br>tersedia di outlet</p>
+                    <p class="text-4xl font-semibold text-[#424954]">100 Item</p>
                 </div>
             </div>
         </div>
